6dd15bfc594d74e3283bf237f1ecfa5771214686
SnehaPant <72789523+SnehaPant@users.noreply.github.com>
2020-12-07 08:56:47 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,23 @@
+class Stone{
+    constructor(x,y,r){
+        var options = {
+            isStatic: false,
+            restitution:0,
+            friction:1,
+            density:1.2
+        }
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+        World.add(world,this.body);
+         this.image=loadImage("stone.png")
+    }
+    display(){
+        var p=this.body.position
+        rectMode(CENTER)
+        ellipse(p.x,p.y,this.r,this.r);
+        imageMode(CENTER); 
+        image(this.image, this.body.position.x,this.body.position.y,30,30);
+      }
+    }
